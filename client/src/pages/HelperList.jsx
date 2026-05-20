91b4e860927f41a77f0f226645df28df2c326d9d
malebolanga <38658699+malebolanga@users.noreply.github.com>
2026-05-20 13:32:48 +0200

    fix-helper-list-loading-crash

--- client/src/pages/HelperList.jsx
+++ client/src/pages/HelperList.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
