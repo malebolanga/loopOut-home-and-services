7b5ac3e6b42db65ab0e2f0458c4b5775b20e69e1
malebolanga <38658699+malebolanga@users.noreply.github.com>
2026-06-23 19:29:14 +0200

    i like it

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "loupeout-home.firebaseapp.com",
-  projectId: "loupeout-home",
-  storageBucket: "loupeout-home.appspot.com",
-  messagingSenderId: "968747879274",
-  appId: "1:968747879274:web:d8c7eab0cd90a10dc13313"
+  apiKey: "AIzaSyCMF4BW6PA0887oPPVda_j1vmpYIHth-rg",
+  authDomain: "central-cinema-218106.firebaseapp.com",
+  databaseURL: "https://central-cinema-218106.firebaseio.com",
+  projectId: "central-cinema-218106",
+  storageBucket: "central-cinema-218106.firebasestorage.app",
+  messagingSenderId: "828543108648",
+  appId: "1:828543108648:web:54ac7894d1fb93d001971d",
+  measurementId: "G-HJDZELETYT"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
